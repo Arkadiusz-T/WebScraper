cb0f24cef267ac7a648db26d57a9642a009ca023
Arkadiusz-design <arkadiusz.tokarski.789@gmail.com>
2020-08-21 10:39:55 +0200

    scraping resources ale contained in a class called Scapo

--- run_scrapper.py
+++ run_scrapper.py
@@ -1,4 +1,4 @@
-import scraping_resources
+from scraping_resources import Scrappo
 
 if __name__ == '__main__':
     print('Welcome to ScraperForFun by Arkadiusz-design')
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     print('Its purpose is to provide backstory of the champions form LeagueOfLegends online game')
     print('and enforce players decisions during "Champion Select" phase of the game to increase a chance of winning')
     print('------------------------------------------------------------------')
+    scrapo = Scrappo()
     keep_script_alive_for_next_iteration = True
     while keep_script_alive_for_next_iteration:
         print('To print list of characters type: 1')
@@ -16,21 +17,22 @@ if __name__ == '__main__':
         chosen_option = input()
 
         if chosen_option == '1':
-            list_of_characters = scraping_resources.download_list_of_characters()
-            [print(name) for name in list_of_characters]
+            [print(name) for name in scrapo.characters_names]
             print('------------------------------------')
 
         if chosen_option == '2':
             print('Type name of a character: ')
             character_name = input()
-            history_of_a_character = scraping_resources.download_history_of_a_character_in_paragraphs(character_name)
-            [print(paragraph) for paragraph in history_of_a_character]
+            history_of_a_character = scrapo.download_history_of_a_character(character_name)
+            print(history_of_a_character)
             print('------------------------------------')
 
         if chosen_option == '3':
             print('in development')
+            print('------------------------------------')
 
         if chosen_option == '4':
+            print('------------------------------------')
             print('in development')
 
         if chosen_option == 'exit':
--- scraping_resources.py
+++ scraping_resources.py
@@ -4,27 +4,45 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-def download_list_of_characters():
-    url_with_characters_names = 'https://na.leagueoflegends.com/en-us/champions/'
-    response_from_the_website = requests.get(url_with_characters_names)
-    parsed_website = BeautifulSoup(response_from_the_website.text, 'html.parser')
-    website_elements_with_names = parsed_website.find_all(class_='style__Text-sc-12h96bu-3 gPUACV')
-    list_of_names = [x.get_text() for x in website_elements_with_names]
-    return list_of_names
+
+class Scrappo():
+
+    def __init__(self):
+        self.characters_names = self._download_list_of_characters_names()
 
 
-def download_history_of_a_character_in_paragraphs(character_name):
-    character_name = character_name.lower()
-    url_with_character_history = 'https://universe.leagueoflegends.com/en_US/story/champion/'+character_name+'/'
-    path_to_driver = r'D:\chrome-webdriver\chromedriver.exe'
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(path_to_driver, chrome_options=chrome_options)
-    driver.get(url_with_character_history)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    time.sleep(3)
-    elements_with_paragraphs = driver.find_elements_by_class_name('p_1_sJ')
-    paragraphs = [x.text for x in elements_with_paragraphs]
-    driver.close()
-    driver.quit()
-    return paragraphs
+    def _download_list_of_characters_names(self):
+        url_with_characters_names = 'https://na.leagueoflegends.com/en-us/champions/'
+        response_from_the_website = requests.get(url_with_characters_names)
+        parsed_website = BeautifulSoup(response_from_the_website.text, 'html.parser')
+        website_elements_with_names = parsed_website.find_all(class_='style__Text-sc-12h96bu-3 gPUACV')
+        list_of_names = [x.get_text() for x in website_elements_with_names]
+        list_of_names = [x.lower() for x in list_of_names]
+        return list_of_names
+
+    def is_this_name_a_champion_name(self, name):
+        if name.lower() in self.characters_names:
+            return True
+        else:
+            return False
+
+    def download_history_of_a_character(self, character_name):
+        character_name = character_name.lower()
+        if self.is_this_name_a_champion_name(character_name):
+            print("Scraping history af a champion, please wait...")
+        else:
+            return 'that name is not a champion name'
+        url_with_character_history = 'https://universe.leagueoflegends.com/en_US/story/champion/'+character_name+'/'
+        path_to_driver = r'D:\chrome-webdriver\chromedriver.exe'
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(path_to_driver, chrome_options=chrome_options)
+        driver.get(url_with_character_history)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(3)
+        elements_with_paragraphs = driver.find_elements_by_class_name('p_1_sJ')
+        paragraphs = [x.text for x in elements_with_paragraphs]
+        history_of_a_champion = '\n'.join(paragraphs)
+        driver.close()
+        driver.quit()
+        return history_of_a_champion
