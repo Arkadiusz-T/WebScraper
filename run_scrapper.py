72b0324520b23adae0100276fc6422a109ecbaef
Arkadiusz-design <arkadiusz.tokarski.789@gmail.com>
2020-08-24 22:35:08 +0200

    scrap counters to a given champion

--- run_scrapper.py
+++ run_scrapper.py
@@ -24,15 +24,18 @@ if __name__ == '__main__':
             print('Type name of a character: ')
             character_name = input()
             history_of_a_character = scrapo.download_history_of_a_character(character_name)
-            print(history_of_a_character)
+            print('Counters: {}'.format(history_of_a_character))
             print('------------------------------------')
 
         if chosen_option == '3':
-            #next to do
-            print('in development')
+            print('What a name of champion do you want to counter: ')
+            name_of_a_champion_to_counter = input()
+            counters_to_a_champion = scrapo.download_counter_to_a_champion(name_of_a_champion_to_counter)
+            print(counters_to_a_champion)
             print('------------------------------------')
 
         if chosen_option == '4':
+            winrate_raport = scrapo.generate_winrate_raport()
             print('------------------------------------')
             print('in development')
 
--- scraping_resources.py
+++ scraping_resources.py
@@ -4,14 +4,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-
 class Scrappo():
 
     def __init__(self):
-        self.characters_names = self._download_list_of_characters_names()
+        self.characters_names = self.__download_list_of_characters_names()
 
 
-    def _download_list_of_characters_names(self):
+    def __download_list_of_characters_names(self):
+        """
+            this method use request library to get list of characters
+            :return: list od all released characters in game
+        """
         url_with_characters_names = 'https://na.leagueoflegends.com/en-us/champions/'
         response_from_the_website = requests.get(url_with_characters_names)
         parsed_website = BeautifulSoup(response_from_the_website.text, 'html.parser')
@@ -21,12 +24,22 @@ class Scrappo():
         return list_of_names
 
     def is_this_name_a_champion_name(self, name):
+        """
+            true if param name is a champion name, else return false
+            :param name: string passed by user
+            :return: bolean
+        """
         if name.lower() in self.characters_names:
             return True
         else:
             return False
 
     def download_history_of_a_character(self, character_name):
+        """
+            this method scrap history of a champion
+            :param character_name: string passed by user
+            :return: string
+        """
         character_name = character_name.lower()
         if self.is_this_name_a_champion_name(character_name):
             print("Scraping history af a champion, please wait...")
@@ -46,3 +59,33 @@ class Scrappo():
         driver.close()
         driver.quit()
         return history_of_a_champion
+
+    def download_counter_to_a_champion(self, name_of_a_champion_to_counter):
+        """
+            get 3 counters of a given champion
+            :param name_of_a_champion_to_counter: string passed by user
+            :return: string
+        """
+        character_name = name_of_a_champion_to_counter.lower()
+        if self.is_this_name_a_champion_name(character_name):
+            print("Scraping counters af a champion, please wait...")
+        else:
+            return 'that name is not a champion name'
+        url_with_statistics = 'https://na.op.gg/champion/' + name_of_a_champion_to_counter + '/statistics/'
+        path_to_driver = r'D:\chrome-webdriver\chromedriver.exe'
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(path_to_driver, chrome_options=chrome_options)
+        driver.get(url_with_statistics)
+        lane_prediction = driver.find_element_by_class_name('champion-stats-header__position__role')
+        print("Counters for champion: {} \nLane: {}".format(name_of_a_champion_to_counter, lane_prediction.text))
+        counters_web_elements = driver.find_elements_by_class_name('champion-stats-header-matchup__table__champion')
+        counters_text = [x.text for x in counters_web_elements]
+        return ', '.join(counters_text)
+
+    def generate_winrate_raport(self):
+        """
+            get 5 best champions for each role
+            :return: dictionary with role name as keys and values as champion names
+        """
+        pass
